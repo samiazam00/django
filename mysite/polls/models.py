5bcdf7896961f87fe7d4266ed729494e8a1f7b0e
Samreen Azam <sa3tnc@virginia.edu>
2020-09-20 21:25:33 -0400

    admin fixes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 #Samreen Azam (sa3tnc)
 from django.utils import timezone
 from django.db import models
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,6 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
